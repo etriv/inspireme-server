9986a24700a41322d651fb08140d33ed64bcdeec
etriv <itai.riv@gmail.com>
2020-01-02 15:33:09 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '123456',
+      database : 'inspireme-db'
+    }
+  });
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = {
+    users: [
+        {
+            id: "123",
+            name: "John",
+            email: "john@gmail.com",
+            password: "asdasd",
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+};
+
+app.get('/', (req, res) => {
+    res.send('Getting root...');
+});
+
+app.post('/signin', (req, res) => {
+    console.log(req.body);
+    if (req.body.email === db.users[0].email && req.body.password === db.users[0].password)
+        res.json('Success');
+    else
+        res.json('Failure');
+});
+
+app.post('/register', (req, res) => {
+    const {email, name, password} = req.body;
+    // Push into db.
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001.');
+});
+
+/* Server functions:
+GET / --> "Getting root..."
+POST /register --> V: user / X: error msg
+POST /sign-in --> V: user / X: error msg
+GET /profile/:id --> user
+
+*/
+
+// app.post('/profile', (req, res) => {
+//     console.log(req.body);
+//     res.send("Got your posted user... Success!");
+// });
+
+// app.get('/profile', (req, res) => {
+//     res.send('<h2>Getting profile...</h2>');
+// });
+
+// app.get('/', (req, res) => {
+//     const user = {
+//         name: "Roger Biano",
+//         Hobby: "Swimming"
+//     }
+//     res.send(user);
+//     //res.send('<h2>Hello!</h2>');
+// });
