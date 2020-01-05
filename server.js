eb57b6a8647a1075a39cadb0b10181b94d467e2b
etriv <itai.riv@gmail.com>
2020-01-05 21:05:37 +0100

    Added insertion of new inspiration to DB. And a DB backup file.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Getting root...');
+    res.send('Getting root... Yam yam yam!');
 });
 
 //////////////////////////////
@@ -45,7 +45,7 @@ app.post('/register', (req, res) => {
             joined: new Date()
         })
         .then(response => {
-            res.json(response);
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -76,7 +76,7 @@ app.get('/profile/:id', (req, res) => {
 // --- INSPIRATIONS --- //
 //////////////////////////////
 
-const allowedTypes = ['video', 'image', 'page'];
+const allowed_types = ['video', 'image', 'page'];
 
 function QueryException(message) {
     this.message = message;
@@ -89,7 +89,7 @@ app.get('/inspirations', (req, res) => {
     db.select('*').from('inspirations')
     .where(builder => {
         if (req.query.hasOwnProperty('type')) {
-            if (allowedTypes.includes(req.query.type))
+            if (allowed_types.includes(req.query.type))
                 builder.where('type', req.query.type);
             else
                 throw new QueryException('Found no matching inspirations with the given type: ' + req.query.type);
@@ -107,7 +107,7 @@ app.get('/inspirations', (req, res) => {
     })
     .then(inspirations => {
         if (inspirations.length)
-            res.json(inspirations);
+            res.status(200).json(inspirations);
         else
             res.status(400).json('No matching inspirations for the query');
     })
@@ -119,6 +119,46 @@ app.get('/inspirations', (req, res) => {
             res.status(400).json('Error while getting inspirations.');
     });
 });
+
+app.post('/inspirations', (req, res) => {
+    console.log('Post body: ',req.body);
+    const { title, source, image, user_id, tags, type } = req.body;
+    // Inserting into db:
+    db('inspirations')
+        .returning('*')
+        .insert({
+            title: title,
+            source: source,
+            image: image,
+            type: type,
+            tags: '{' + tags + '}',
+            user_id: user_id,
+            added: new Date()
+        })
+        .then(response => {
+            res.status(200).json("Inspiration successfully uploaded.");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json("Failed uploading inspiration.");
+        });
+});
+
+/////////////////////////////////
+// --- STARTING THE SERVER --- //
+/////////////////////////////////
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001.');
+});
+
+/* 
+Server functions:
+GET / --> "Getting root..."
+POST /register --> V: user / X: error msg
+POST /sign-in --> V: user / X: error msg
+GET /profile/:id --> user
+*/
 
 // *** Old code to handle the existence of a Types table
 // // Mapping TYPES to thier corrsponding ID
@@ -152,65 +192,3 @@ app.get('/inspirations', (req, res) => {
 //         res.status(400).json('Error while getting inspirations');
 //     });
 // });
-
-// app.get('/inspirations', (req, res) => {
-//     console.log(req.query);
-
-//     db.select('*').from('inspirations')
-//     .then(inspirations => {
-//         if (inspirations.length)
-//             res.json(inspirations);
-//         else
-//             res.status(400).json('Found no inspirations with type: ' + type);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(400).json('Error while getting inspirations');
-//     });
-// });
-
-// app.get('/inspirations/:type', (req, res) => {
-//     const { type } = req.params;
-//     db.select('*').from('inspirations').where('type', type)
-//         .then(inspirations => {
-//             if (inspirations.length)
-//                 res.json(inspirations);
-//             else
-//                 res.status(400).json('Found no inspirations with type: ' + type);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(400).json('Error while getting inspirations');
-//         });
-// });
-
-// app.get('/inspirations/:type', (req, res) => {
-//     const { type } = req.params;
-//     db.select('*').from('inspirations').where('type', type)
-//         .then(inspirations => {
-//             if (inspirations.length)
-//                 res.json(inspirations);
-//             else
-//                 res.status(400).json('Found no inspirations with type: ' + type);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(400).json('Error while getting inspirations');
-//         });
-// });
-
-/////////////////////////////////
-// --- STARTING THE SERVER --- //
-/////////////////////////////////
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3001.');
-});
-
-/* Server functions:
-GET / --> "Getting root..."
-POST /register --> V: user / X: error msg
-POST /sign-in --> V: user / X: error msg
-GET /profile/:id --> user
-
-*/
